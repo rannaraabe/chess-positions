9d5bb23066924f34dc088f69e3580e0f808f7439
Ranna Raabe <ranna.raabe@gmail.com>
2020-11-02 22:49:02 -0300

    Delete renomear.py

--- renomear.py
+++
@@ -1,8 +0,0 @@
-import os
-
-prefix = 'nada_'
-
-for file in os.listdir("./dataset/images/"):
-    filename = prefix+file
-    os.rename("./dataset/images/"+file, "./dataset/images/"+filename)
-    print(file)
